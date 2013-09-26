47887e20fe46aff3d26698c4160f8337573dd22a
Ashley Basinger <ashley.hipps@gmail.com>
2013-09-26 15:00:24 -0400

    added comment in new.html.erb for refactor idea

---
+++ spec/features/record_building_owner_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+feature 'record building owner', %Q{
+  As a real estate associate
+  I want to record a building owner
+  So that I can keep track of our relationships with owners
+}
